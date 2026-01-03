db19de962a4b798406d1bd09638469aeb75036ed
vivekranpara18 <vivekranpara018@gmail.com>
2026-01-03 22:37:09 +0530

    Add files via upload

---
+++ 1_list.py
@@ -0,0 +1,11 @@
+place=["salangpur_hanumanji_mandir"]
+
+value=["vivek",10,3.14,316,"start",55.14,789,"true",163,54]
+
+print(place)
+print(value)
+print(value[0])
+print(value[0:5])
+print(value[5:])
+print(value*2)
+print(value[1:5])
