5cef7919be275c8f590b8b424ae7c25a9c1c65ce
vivekranpara18 <vivekranpara018@gmail.com>
2026-01-04 19:33:18 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,19 @@
+#create variable and store and display
+
+name="vivek"
+address="chitra"
+city="bhavnagar"
+pincode="364004"
+age="20"
+weight="62"
+height="172"
+gender="male"
+
+print(name)
+print(address)
+print(city)
+print(pincode)
+print(age)
+print(weight)
+print(height)
+print(gender)
