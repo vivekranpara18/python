0e4c846ccbc39e2030af0c340609cfea875ebf36
vivekranpara18 <vivekranpara018@gmail.com>
2026-01-07 12:00:49 +0530

    Add files via upload

---
+++ 12_set_1.py
@@ -0,0 +1,6 @@
+fruits = {"apple", "banana", "mango", "orange", "grapes", "pineapple", "papaya", "kiwi", "strawberry", "watermelon"}
+fruits.add('apple')
+fruits.add('mango')
+fruits.add('coconut')
+fruits.remove('papaya')
+print(fruits)
