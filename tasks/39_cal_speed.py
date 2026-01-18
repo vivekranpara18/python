53ffd9ae0fac55682d41d24950a88e404868fbca
vivekranpara18 <vivekranpara018@gmail.com>
2026-01-18 19:52:07 +0530

    Add files via upload

---
+++ tasks/39_cal_speed.py
@@ -0,0 +1,8 @@
+#Write a program to calculate speed using distance and time.
+
+time = int(input("enter time :"))
+distance = int(input("enter distance :"))
+
+speed = distance / time
+
+print("speed is :",speed)
---
+++ tasks/40_meter_to_kilometer.py
@@ -0,0 +1,6 @@
+#Write a program to convert meters into kilometers.
+
+meter = float(input("enter meter :"))
+
+kilometer = meter / 1000
+print("kilometer is",kilometer)
