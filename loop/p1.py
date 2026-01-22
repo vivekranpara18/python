032368d28a583391ccc0ab3945074df6fef0baa6
vivekranpara18 <vivekranpara018@gmail.com>
2026-01-22 12:16:05 +0530

    Add files via upload

---
+++ loop/p1.py
@@ -0,0 +1,6 @@
+# write a program to print following series 
+# 1     3       5       7       9       11      13      .... 100
+number = 1
+while number < 100:
+    print(number,end=' ')
+    number = number + 2
