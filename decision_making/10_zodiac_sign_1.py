657b8665910655c2f564d7e42025b86e4313d3f8
vivekranpara18 <vivekranpara018@gmail.com>
2026-01-22 19:05:52 +0530

    Update 10_zodiac_sign_1.py

--- decision_making/10_zodiac_sign_1.py
+++ decision_making/10_zodiac_sign_1.py
@@ -22,37 +22,39 @@ if (month == 3 and day >= 21) or (month == 4 and day <= 19):
 elif (month == 4 and day >= 20) or (month == 5 and day <=20):
     print("taurus")
 
-elif (month == 5 and day <= 21) or (month == 6 and day <= 21):
+elif (month == 5 and day >= 21) or (month == 6 and day <= 21):
     print("gemini")
 
-elif (month == 6 and day <= 22) or (month == 7 and day <= 22):
+elif (month == 6 and day >= 22) or (month == 7 and day <= 22):
     print("cancer")
 
-elif (month == 7 and day <= 23) or (month == 8 and day <= 22):
+elif (month == 7 and day >= 23) or (month == 8 and day <= 22):
     print("leo")
 
-elif (month == 8 and day <= 23) or (month == 9 and day <= 22):
+elif (month == 8 and day >= 23) or (month == 9 and day <= 22):
     print("virgo")
 
-elif(month==9 and day==23) or (month==10 and day==22):
+elif (month == 9 and day >= 23) or (month == 10 and day <= 22):
     print("libra")
 
-elif(month==10 and day==24) or (month==11 and day==21):
+elif (month == 10 and day >= 24) or (month == 11 and day <= 21):
     print("scorpio")
 
-elif(month==11 and day==22) or (month==12 and day==21):
+elif (month == 11 and day >= 22) or (month == 12 and day <= 21):
     print("Sagittarius")
 
-elif(month==12 and day==22) or (month==1 and day==19):
+elif (month == 12 and day >= 22) or (month == 1 and day <= 19):
     print("Capricorn")
 
-elif(month==1 and day==20) or (month==2 and day==18):
+elif (month == 1 and day >= 20) or (month == 2 and day <= 18):
     print("Aquarius")
 
-elif(month==2 and day==19) or (month==3 and day==20):
+elif (month == 2 and day >= 19) or (month == 3 and day <= 20):
     print("Pisces")
 
 else:
     print("invlaid data")
+
+
 
     
