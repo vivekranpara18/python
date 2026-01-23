a085141f6cde5f5a123ec0326d25072ae103cae3
vivekranpara18 <vivekranpara018@gmail.com>
2026-01-23 11:44:38 +0530

    Add files via upload

---
+++ loop/p5.py
@@ -0,0 +1,9 @@
+#print series 1    8   27   125  ....... 1000
+
+number = 1
+cube = 0
+print(number,end=' ')
+while number<10:
+    number = number + 1
+    cube = number * number * number
+    print(cube,end=' ')
