9555893f196de59bec9dcf42db305dbd61efbf6a
vivekranpara18 <vivekranpara018@gmail.com>
2026-01-27 11:33:33 +0530

    Add files via upload

---
+++ loop/p12_for.py
@@ -0,0 +1,7 @@
+# for loop with list 
+# write a program to display only positive value in list 
+list = [12, -45, 7, -3, 89, -22, 0, 34, -9, 56]
+
+for num in list:
+    if num>0:
+        print(num)
