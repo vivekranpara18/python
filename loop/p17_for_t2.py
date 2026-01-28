08df702dce003fe2a1084039f559ee0aac2b07cc
vivekranpara18 <vivekranpara018@gmail.com>
2026-01-28 18:04:05 +0530

    Add files via upload

---
+++ loop/p17_for_t2.py
@@ -0,0 +1,7 @@
+# write a program to generate and display sum of all the float values in tuple 
+# and also calculate average
+values = (1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.0)
+sum = 0
+for item in values:
+    sum = sum + item
+print(sum)
