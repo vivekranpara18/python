83ce602a2ac4b33e92fb3b1c41467b6ff75dcd12
vivekranpara18 <vivekranpara018@gmail.com>
2026-02-02 20:43:42 +0530

    Refactor comments and formatting in simple_interest function

--- loop/function/function_3.py
+++ loop/function/function_3.py
@@ -2,10 +2,10 @@
 # return simple interest of given amount rate and year
 
 def simple_interest(p,r,n):
-    return (p*r*n)/100
-
+    return (p*r*n)/100   
 p=int(input("enter price :"))
 r=int(input("enter rate :"))
 n=int(input("enter year :"))
 si=simple_interest(p,r,n)
+
 print(si)
