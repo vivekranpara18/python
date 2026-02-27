e23beee89e5c481008667051ceee06b9bbe21d06
vivekranpara18 <vivekranpara018@gmail.com>
2026-02-27 10:33:48 +0530

    Add files via upload

---
+++ module/module_3.py
@@ -0,0 +1,11 @@
+import math
+number = 3.39
+
+print(f"celi value of {number} :",math.ceil(number))
+print(f"floor value of {number} :",math.floor(number))
+print(f"truncate value of {number} :",math.trunc(number))
+print(f"truncate value of {number} :",math.fabs(-5))
+print("factorial of 5 :",math.factorial(5))
+print(f"copysign of {number} :",math.copysign(number,1))
+print(f"output of modf :",math.modf(number))
+print(f"reminder of 10 divided by 3 :",math.fmod(10,3))
