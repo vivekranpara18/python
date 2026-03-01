e7b88870d8e71e23d284bcaa5e3cc37a1d65dcbf
vivekranpara18 <vivekranpara018@gmail.com>
2026-03-01 14:56:27 +0530

    Add files via upload

---
+++ module/currency.py
@@ -0,0 +1,23 @@
+'''create module currency.py which has below methods 
+    toDollar which convert given Rupees into Dollar 
+    toEuro  which convert given Rupees into Euro 
+    toPound which convert given Rupees into Pound 
+    ToYen which convert given Rupees into Yen'''
+
+def todoller(doller):
+    rupees=doller/92
+    return rupees
+
+def toeuro(euro):
+    rupees=euro/106
+    return rupees
+
+def topound(pound):
+    rupees=pound/122
+    return rupees
+
+def toyen(yen):
+    rupees=yen/0.59
+    return rupees
+
+
