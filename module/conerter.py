41ef7a028dbaa5aaf8494ecec198fa843bf61e0a
vivekranpara18 <vivekranpara018@gmail.com>
2026-03-02 19:58:10 +0530

    Add files via upload

---
+++ module/conerter.py
@@ -0,0 +1,18 @@
+#create function to convert meter into inches
+
+def metertoinch(meter):
+    inch=meter*39.37
+    return inch
+
+#create function to convert feet into inches
+
+def foottoinch(foot):
+    inch=foot*12
+    return inch
+
+def cmtoinch(cm):
+    inch=cm/2.54
+    return inch
+
+
+
