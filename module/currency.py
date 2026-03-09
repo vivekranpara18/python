15ca60b6b065f2963219a7a6d4ddcbd6b8738efa
vivekranpara18 <vivekranpara018@gmail.com>
2026-03-09 19:21:57 +0530

    Add files via upload

--- module/currency.py
+++ module/currency.py
@@ -1,23 +1,21 @@
-'''create module currency.py which has below methods 
-    toDollar which convert given Rupees into Dollar 
-    toEuro  which convert given Rupees into Euro 
-    toPound which convert given Rupees into Pound 
-    ToYen which convert given Rupees into Yen'''
+#create module currency.py which has below methods 
+#    toDollar which convert given Rupees into Dollar 
+#    toEuro  which convert given Rupees into Euro 
+#    toPound which convert given Rupees into Pound 
+#    ToYen which convert given Rupees into Yen
 
-def todoller(doller):
-    rupees=doller/92
-    return rupees
+def todollar(rupees):
+    dollar = rupees/92
+    return dollar
 
-def toeuro(euro):
-    rupees=euro/106
-    return rupees
+def toeuro(rupees):
+    euro = rupees/106
+    return euro
 
-def topound(pound):
-    rupees=pound/122
-    return rupees
+def topound(rupees):
+    pound = rupees/124
+    return pound
 
-def toyen(yen):
-    rupees=yen/0.59
-    return rupees
-
-
+def toyen(rupees):
+    yen = rupees/0.60
+    return yen
