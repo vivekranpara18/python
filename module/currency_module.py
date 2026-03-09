25b8d33b16118322f4592635cdb2b07abb82f69e
vivekranpara18 <vivekranpara018@gmail.com>
2026-03-09 19:22:24 +0530

    Add files via upload

---
+++ module/currency_module.py
@@ -0,0 +1,8 @@
+import currency
+
+rupees = float(input("enter rupees :"))
+
+print('Dollar :',currency.todollar(rupees))
+print('Euro :',currency.toeuro(rupees))
+print('Pound :',currency.topound(rupees))
+print('Yen :',currency.toyen(rupees))
