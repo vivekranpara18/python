bb310479f32c25e5b8c472da754806ea2af914e9
vivekranpara18 <vivekranpara018@gmail.com>
2026-03-10 12:26:38 +0530

    Enhance string_to_number with error handling
    Added error handling for invalid input in string_to_number function.

--- loop/function/string_to_number.py
+++ loop/function/string_to_number.py
@@ -1,5 +1,9 @@
 def string_to_number(number):
-    return int(number)
+    try:
+        return int(number)
+    except(ValueError):
+        return("input is not valid")
 
 n1=input("enter string :")
 print(string_to_number(n1))
+
